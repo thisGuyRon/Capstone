0f0af801579b02d1fad9a1a0162ba14f9a229579
thisGuyRon <42878005+thisGuyRon@users.noreply.github.com>
2019-05-01 22:53:16 -0400

    Dynamic/map added 5-1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import sys
 import pandas as pd
 import numpy as np
 
@@ -58,23 +58,41 @@ def springfield():
     cur.execute("select * from spring_new")
     rv = cur.fetchall()
     return jsonify(rv)
-@app.route('/season_load')
-def season_load():
-    cur.execute('select "All" union select distinct season from episodes')
-    rv=cur.fetchall()
-    return jsonify(rv)
+#@app.route('/season_load')
+#def season_load():
+#    cur.execute('select "All" union select distinct season from episodes')
+#    rv=cur.fetchall()
+#    return jsonify(rv)
 
-@app.route('/char_load')
-def char_load():
-    cur.execute('select * from char_dropdown')
-    rv1=cur.fetchall()
-    return jsonify(rv1)
+#@app.route('/char_load')
+#def char_load():
+#    cur.execute('select * from char_dropdown')
+#    rv1=cur.fetchall()
+#   return jsonify(rv1)
 
 @app.route('/search/<input>')
 def search(input):
     cur.execute('select season as "Season_No", season_episode_no as "Season_Episode_No", title, "" as "line", image_url as "image" from episodes where title like "%' + input + '%" union select e.season, e.season_episode_no, e.title, s.normalized_text, e.image_url from episodes e join s_lines s on e.episode_id=s.episode_id where s.normalized_text like "%'+input+'%" limit 500')
     rv=cur.fetchall()
     return jsonify(rv)
+
+@app.route('/character/<char>/<sea>')
+def character(char, sea):
+    
+    print(char, file=sys.stdout)
+    print(sea, file=sys.stdout)
+    if sea == "All":
+       
+        cur.execute('select e.season, cast(ifnull(sum(a.Lines), 0) as unsigned) as "Lines", cast(ifnull(sum(a.Words), 0) as unsigned) as "Words" from episodes e left join (select s.episode_id, count(s.line_id) as "Lines", sum(s.word_count) "Words" from s_lines s join characters c on s.character_id=c.char_id where c.name="' + char + '" group by s.episode_id) a on e.episode_id=a.episode_id group by e.season order by e.season asc')
+        rv=cur.fetchall()
+    else:
+       
+        cur.execute('select e.season_episode_no as "Episode", ifnull(a.Lines, 0) as "Lines", cast(ifnull(a.Words, 0) as unsigned) as "Words" from episodes e left join (select s.episode_id, count(s.line_id) as "Lines", sum(s.word_count) "Words" from s_lines s join characters c on s.character_id=c.char_id where c.name="' + char + '" group by s.episode_id) a on e.episode_id=a.episode_id where e.season="' + sea + '"order by e.season_episode_no')
+        rv=cur.fetchall()
+        for row in rv:
+            print(row, file=sys.stdout)
+    return jsonify(rv)
+
 
 
 if __name__ == "__main__":
--- static/js/Dynamic.js
+++ static/js/Dynamic.js
@@ -1,3 +1,64 @@
+var seasons = [
+ {seasons:"All"},
+ {seasons:"1"},
+ {seasons:"2"},
+ {seasons:"3"},
+ {seasons:"4"},
+ {seasons:"5"},
+ {seasons:"6"},
+ {seasons:"7"},
+ {seasons:"8"},
+ {seasons:"9"},
+ {seasons:"10"},
+ {seasons:"11"},
+ {seasons:"12"},
+ {seasons:"13"},
+ {seasons:"14"},
+ {seasons:"15"},
+ {seasons:"16"},
+ {seasons:"17"},
+ {seasons:"18"},
+ {seasons:"19"},
+ {seasons:"20"},
+ {seasons:"21"},
+ {seasons:"22"},
+ {seasons:"23"},
+ {seasons:"24"},
+ {seasons:"25"},
+ {seasons:"26"},
+ {seasons:"27"}
+ 
+];
+//console.log(seasons);
+var characters = [
+    {name:"Homer Simpson"},
+    {name:"Marge Simpson"},
+    {name:"Bart Simpson"},
+    {name:"Lisa Simpson"},
+    {name:"C. Montgomery Burns"},
+    {name:"Moe Szyslak"},
+    {name:"Seymour Skinner"},
+    {name:"Ned Flanders"},
+    {name:"Grampa Simpson"},
+    {name:"Milhouse Van Houten"},
+    {name:"Chief Wiggum"},
+    {name:"Krusty the Clown"},
+    {name:"Nelson Muntz"},
+    {name:"Lenny Leonard"},
+    {name:"Apu Nahasapeemapetilon"},
+    {name:"Waylon Smithers"},
+    {name:"Kent Brockman"},
+    {name:"Carl Carlson"},
+    {name:"Edna Krabappel-Flanders"},
+    {name:"Dr. Julius Hibbert"},
+    {name:"Selma Bouvier"},
+    {name:"Barney Gumble"},
+    {name:"Rev. Timothy Lovejoy"},
+    {name:"Sideshow Bob"},
+    {name:"Gary Chalmers"}
+];
+var char1 = d3.select(".btn-char1");
+var char2 = d3.select(".btn-char2");
 function init(){
     
     load_seasons();
@@ -7,35 +68,289 @@ function init(){
     
     
 }
-function load_seasons(){
-    d3.json("/season_load", function(data){
+async function load_seasons(){
+    //d3.json("/season_load", function(data){
         var enter_season = d3.select("#dropdown-3");
 
-        console.log(data);
-        for(x=0; data.length; x++){
-            //console.log(data[x].All)
+      // console.log(data);
+       console.log(seasons);
+      // for(x=0; x<data.length; x++){
+        for(x=0; x<seasons.length; x++){
+            //console.log(seasons[x].seasons)
             enter_season
                 .append("option")
-                .text(data[x].All)
-                .property("value", data[x].All);
+                .text(seasons[x].seasons)
+                .property("value", seasons[x].seasons);
         }
-    });
-}
-function load_char(){
-    d3.json("/char_load", function(char_data){
+       // load_char();
+  //  })
+   }
+
+async function load_char(){
+    //await load_seasons();
+ //  d3.json("/char_load", function(char_data){
         var char1 = d3.select("#dropdown-1");
         var char2 = d3.select("#dropdown-2");
-        console.log(char_data);
-        for(x=0; char_data.length; x++){
+        console.log(characters);
+       // for(x=0; x<char_data.length; x++){
+        for(x=0; x<characters.length; x++){
+            //console.log(char_data[x].name);
             char1
                 .append("option")
-                .text(char_data[x].name)
-                .property("value", char_data[x].name);
+                .text(characters[x].name)
+                .property("value", characters[x].name);
             char2
                 .append("option")
-                .text(char_data[x].name)
-                .property("value", char_data[x].name);
+                .text(characters[x].name)
+                .property("value", characters[x].name);
         }
-    })
+  //  })
 }
 init();
+
+char1.on("click", char1_load);
+char2.on("click", char2_load);
+
+function char1_load(){
+    d3.event.preventDefault();
+    var seasonInput=d3.select("#dropdown-3");
+    var seasonValue = seasonInput.property("value");
+    var char1Input = d3.select("#dropdown-1");
+    var char1Value = char1Input.property("value");
+    var char1JS=encodeURIComponent(char1Value.trim())
+    var char1_url = "/character/" + char1JS +"/"+seasonValue;
+    
+   d3.json(char1_url, function(character1){
+       if (seasonValue==="All"){
+            var season=[];
+            var seasonLineCount=[];
+            var seasonWordCount=[];
+            var lineTotal=0;
+            var wordTotal=0;
+            var lineMax=0;
+            var wordMax=0;
+            var appearances=0;
+            var lineAverage;
+            var wordAverage;
+            for(x=0;x<character1.length;x++){
+                var line = character1[x].Lines;
+                var word = character1[x].Words;
+                lineTotal = lineTotal+character1[x].Lines;
+                wordTotal = wordTotal+character1[x].Words;
+                if(line>0){
+                    appearances++;
+                };
+            if(line>lineMax){
+                lineMax = line;
+            }
+            if(word>wordMax){
+                wordMax=word;
+            };
+                season.push(character1[x].season);
+                seasonLineCount.push(character1[x].Lines);
+                seasonWordCount.push(character1[x].Words);
+            }
+            wordAverage = wordTotal/appearances;
+            lineAverage = lineTotal/appearances;
+            console.log(wordAverage);
+            console.log(lineAverage);
+            console.log(lineMax);
+            console.log(wordMax);
+            console.log(appearances);
+            console.log(lineTotal);
+            console.log(wordTotal); 
+            var trace1 = {
+                x: season,
+                y: seasonLineCount,
+                type: 'scatter'
+            };
+            Plotly.newPlot('char1-graph-lines', [trace1]);
+
+            var trace2 = {
+                x: season,
+                y: seasonWordCount,
+                type: 'scatter'
+            };
+            Plotly.newPlot('char1-graph-words', [trace2]);
+
+       }
+       else{
+        var episodes=[];
+        var episodeLineCount=[];
+        var episodeWordCount=[];
+        var lineTotal=0;
+        var wordTotal=0;
+        var lineMax=0;
+        var wordMax=0;
+        var appearances=0;
+        var lineAverage;
+        var wordAverage;
+        for(x=0;x<character1.length;x++){
+            var line = character1[x].Lines;
+            var word = character1[x].Words;
+            lineTotal = lineTotal+character1[x].Lines;
+            wordTotal = wordTotal+character1[x].Words;
+            if(line>0){
+                appearances++;
+            };
+            if(line>lineMax){
+                lineMax = line;
+            }
+            if(word>wordMax){
+                wordMax=word;
+            };
+            episodes.push(character1[x].Episode);
+            episodeLineCount.push(character1[x].Lines);
+            episodeWordCount.push(character1[x].Words);
+        }
+        wordAverage = wordTotal/appearances;
+        lineAverage = lineTotal/appearances;
+        console.log(wordAverage);
+        console.log(lineAverage);
+        console.log(lineMax);
+        console.log(wordMax);
+        console.log(appearances);
+        console.log(lineTotal);
+        console.log(wordTotal);
+        var trace1 = {
+            x: episodes,
+            y: episodeLineCount,
+            type: 'scatter'
+        };
+        Plotly.newPlot('char1-graph-lines', [trace1]);
+
+        var trace2 = {
+            x: episodes,
+            y: episodeWordCount,
+            type: 'scatter'
+        };
+        Plotly.newPlot('char1-graph-words', [trace2]);
+       }
+        console.log(character1[0].Episode);
+        console.log(season);
+        console.log(seasonLineCount);
+        console.log(seasonWordCount);
+        
+    })
+    
+    
+}
+function char2_load(){
+    d3.event.preventDefault();
+    var seasonInput=d3.select("#dropdown-3");
+    var seasonValue = seasonInput.property("value");
+    var char2Input = d3.select("#dropdown-2");
+    var char2Value = char2Input.property("value");
+    var char2JS=encodeURIComponent(char2Value.trim())
+    console.log(char2JS);
+    console.log(seasonValue);
+    var char2_url = "/character/" + char2JS +"/"+seasonValue;
+    
+   d3.json(char2_url, function(character2){
+        console.log(character2);
+        if (seasonValue==="All"){
+            var season=[];
+            var seasonLineCount=[];
+            var seasonWordCount=[];
+            var lineTotal=0;
+            var wordTotal=0;
+            var lineMax=0;
+            var wordMax=0;
+            var appearances=0;
+            var lineAverage;
+            var wordAverage;
+                for(x=0;x<character2.length;x++){
+                    var line = character2[x].Lines;
+                    var word = character2[x].Words;
+                    lineTotal = lineTotal+character2[x].Lines;
+                    wordTotal = wordTotal+character2[x].Words;
+                    if(line>0){
+                        appearances++;
+                    };
+                    if(line>lineMax){
+                        lineMax = line;
+                    }
+                    if(word>wordMax){
+                        wordMax=word;
+                    }
+    
+                    season.push(character2[x].season);
+                    seasonLineCount.push(character2[x].Lines);
+                    seasonWordCount.push(character2[x].Words);
+                }
+                wordAverage = wordTotal/appearances;
+                lineAverage = lineTotal/appearances;
+                console.log(wordAverage);
+                console.log(lineAverage);
+                console.log(lineMax);
+                console.log(wordMax);
+                console.log(appearances);
+                console.log(lineTotal);
+                console.log(wordTotal); 
+                var trace1 = {
+                    x: season,
+                    y: seasonLineCount,
+                    type: 'scatter'
+                };
+                Plotly.newPlot('char2-graph-lines', [trace1]);
+    
+                var trace2 = {
+                    x: season,
+                    y: seasonWordCount,
+                    type: 'scatter'
+                };
+                Plotly.newPlot('char2-graph-words', [trace2]);
+    
+           }
+           else{
+            var episodes=[];
+            var episodeLineCount=[];
+            var episodeWordCount=[];
+            var lineTotal=0;
+            var wordTotal=0;
+            var lineMax=0;
+            var wordMax=0;
+            var appearances=0;
+            var lineAverage;
+            var wordAverage;
+            for(x=0;x<character2.length;x++){
+                var line = character2[x].Lines;
+                var word = character2[x].Words;
+                lineTotal = lineTotal+character2[x].Lines;
+                wordTotal = wordTotal+character2[x].Words;
+                if(line>0){
+                    appearances++;
+                };
+                if(line>lineMax){
+                    lineMax = line;
+                }
+                if(word>wordMax){
+                    wordMax=word;
+                }
+                episodes.push(character2[x].Episode);
+                episodeLineCount.push(character2[x].Lines);
+                episodeWordCount.push(character2[x].Words);
+            }
+            console.log(wordAverage);
+            console.log(lineAverage);
+            console.log(lineMax);
+            console.log(wordMax);
+            console.log(appearances);
+            console.log(lineTotal);
+            console.log(wordTotal);
+            var trace1 = {
+                x: episodes,
+                y: episodeLineCount,
+                type: 'scatter'
+            };
+            Plotly.newPlot('char2-graph-lines', [trace1]);
+    
+            var trace2 = {
+                x: episodes,
+                y: episodeWordCount,
+                type: 'scatter'
+            };
+            Plotly.newPlot('char2-graph-words', [trace2]);
+           }
+    })
+}
